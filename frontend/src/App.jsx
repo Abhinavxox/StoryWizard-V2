d34c114878b232b5b35d63f40feb7f65e864854a
Abhinav Pandey <abhinavxox@gmail.com>
2023-06-30 11:12:04 +0530

    Add : Added boiler plate, react router, daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./layout/Home";
 
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
