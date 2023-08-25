0e7788b3442dc3de4ae425fc1ce9804c9cd5fcd5
Abhinavxox <abhinavxox@gmail.com>
2023-08-25 13:18:59 +0545

    Fix : Fixed the media bar component

--- frontend/src/components/MediaBar.jsx
+++ frontend/src/components/MediaBar.jsx
@@ -23,6 +23,16 @@ const MediaBar = () => {
     setIsHovered((prev) => !prev);
   };
 
+  const handlePlayPause = () => {
+    if (audioRef.current.paused) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
   useEffect(() => {
     if (!audio) {
       if (error) {
@@ -36,9 +46,48 @@ const MediaBar = () => {
 
   return (
     <>
-      <div className="flex justify-center">
-        <audio ref={audioRef} controls />
-      </div>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div
+          className={`relative ${
+            isHovered ? "w-32 h-14" : "w-14 h-14"
+          } rounded-full cursor-pointer overflow-hidden transition-all`}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHover}
+        >
+          <div
+            className={`absolute top-0 left-0 w-full h-full flex justify-between items-center ${
+              isHovered ? "animate-expand-height" : "animate-shrink-height"
+            }`}
+          >
+            {isHovered ? (
+              <>
+                <button
+                  className="w-14 h-14 bg-base-300 text-white rounded-full"
+                  onClick={handlePlayPause}
+                >
+                  <div className="flex justify-center">
+                    {isPlaying ? <FaPause /> : <FaPlay />}
+                  </div>
+                </button>
+                <button className="w-14 h-14 bg-base-300 text-white rounded-full">
+                  <div className="flex justify-center">
+                    <FaQuestion />
+                  </div>
+                </button>
+              </>
+            ) : (
+              <button className="w-14 h-14 bg-base-300 text-white rounded-full">
+                <div className="flex justify-center">
+                  <FaTimes />
+                </div>
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+      <audio ref={audioRef} controls hidden />
     </>
   );
 };
