88471144c96c9904bebff4a527ec92cdb9b6aa4c
Abhinavxox <abhinavxox@gmail.com>
2023-08-27 22:36:09 +0545

    Fix : Story card fixed

--- frontend/src/components/StoryCard.jsx
+++ frontend/src/components/StoryCard.jsx
@@ -7,11 +7,11 @@ const StoryCard = ({ heading, content, imgSrc, id }) => {
         <div className="relative transition-all duration-500 ease-in-out transform group-hover:scale-110">
           <img
             src={imgSrc}
-            className="w-full h-64 rounded-lg "
+            className="w-full h-64 rounded-t-lg "
             alt={heading}
             draggable="false"
           />
-          <div className="absolute card w-full bottom-0 rounded-lg">
+          <div className="w-full bottom-0 rounded-b-lg bg-gray-600 bg-opacity-10 p-5">
             <h2 className="text-2xl p-1 font-bold">{heading}</h2>
             <p className="p-2 truncate line-clamp-3">{content}</p>
           </div>
